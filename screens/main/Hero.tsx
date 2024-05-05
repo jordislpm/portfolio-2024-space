e1a693b52f514a2d5bfd97294b298d83c6302757
jordislpm <jordislpm@gmail.com>
2024-05-04 23:05:15 -0400

    fix: add style for hole in main with css pure

--- screens/main/Hero.tsx
+++ screens/main/Hero.tsx
@@ -3,12 +3,12 @@ import HeroContent from '@/components/Sub/HeroContent'
 
 function Hero() {
   return (
-    <div className='relative flex flex-col h-full w-full'>
+    <div className='relative flex flex-col h-full w-full -z-40'>
         <video 
         autoPlay
         muted
         loop
-        className='rotate-180 absolute top-[-340px] h-full w-full left-0 z-[-10] object-cover'>
+        className='video_hole rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover'>
 <source src='/blackhole.webm' type='video/webm'/>
         </video>
         <HeroContent/>
