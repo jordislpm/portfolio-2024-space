2940163790a5785efebcd61784026efc47d32e89
jordis.dev <jordislpm@gmail.com>
2025-08-15 20:53:12 -0400

    reorder projects

--- constants/projects.ts
+++ constants/projects.ts
@@ -2,52 +2,14 @@ import PopUpAlertProjectLink from "@/components/PopUpAlertProjectLink";
 import { typeProjectCard } from "@/types";
 
 export const myProjects: typeProjectCard[] = [
-  {
-    company: "Nevobit",
-    src: "/projects/itaaj-realty.png",
-    title: "Itaaj realty: Real state page",
-    description:
-      "As part of my work at Nevobit in 2023, my initial assignment involved developing a comprehensive front-end interface. This website is designed for users to buy, sell, and rent real estate properties.",
-    repository: "https://github.com/nevobit/itaaj-monorepo",
-    liveCode: "https://www.itaajrealty.com/",
-    technologies: [
-      "HTML",
-      "CSS",
-      "TypeScript",
-      "React",
-      "Next 13",
-      "Redux Toolkit",
-    ],
-    stackType: "frontend",
-  },
-  {
-    company: "Nevobit",
-    src: "/projects/helebba.png",
-    title: "Helebba: Business administration",
-    description:
-      "A software for managing invoicing, accounting, inventory, CRM, projects, and HR from one platform. At Nevobit, I played a key role in implementing front-end and took on some basic back-end tasks.",
-    repository: "https://github.com/nevobit/helebba-monorepo",
-    liveCode: "https://www.helebba.com/",
-    technologies: [
-      "HTML",
-      "CSS",
-      "TypeScript",
-      "React",
-      "React Toolkit",
-      "Next 13",
-      "Node js",
-    ],
-    stackType: "frontend",
-    alert: true,
-  },
    {
   company: "personal",
   src: "/projects/ecommerce-oli-store.png",
   title: "Oli Store E-commerce",
   description:
     "Custom e-commerce platform developed with a modern frontend in React/Next.js and a headless backend powered by Wix. Includes product catalog, shopping cart, checkout, and payment integration, designed with a clean and minimalistic UI for a professional online store experience.",
-  repository: "https://github.com/jordislpm/ecommerce-clone-template-spanish", 
-  liveCode: "https://ecommerce-clone-template-spanish.vercel.app/", 
+  repository: "https://github.com/jordislpm/oli_store", 
+  liveCode: "https://oli-store.vercel.app/", 
   technologies: [
     "Next.js",
     "React",
@@ -114,5 +76,43 @@ export const myProjects: typeProjectCard[] = [
     liveCode: "https://poke-app-ebon-zeta.vercel.app/",
     technologies: ["HTML", "CSS", "JavaScript", "React"],
     stackType: "frontend",
+  },
+    {
+    company: "Nevobit",
+    src: "/projects/itaaj-realty.png",
+    title: "Itaaj realty: Real state page",
+    description:
+      "As part of my work at Nevobit in 2023, my initial assignment involved developing a comprehensive front-end interface. This website is designed for users to buy, sell, and rent real estate properties.",
+    repository: "https://github.com/nevobit/itaaj-monorepo",
+    liveCode: "https://www.itaajrealty.com/",
+    technologies: [
+      "HTML",
+      "CSS",
+      "TypeScript",
+      "React",
+      "Next 13",
+      "Redux Toolkit",
+    ],
+    stackType: "frontend",
+  },
+  {
+    company: "Nevobit",
+    src: "/projects/helebba.png",
+    title: "Helebba: Business administration",
+    description:
+      "A software for managing invoicing, accounting, inventory, CRM, projects, and HR from one platform. At Nevobit, I played a key role in implementing front-end and took on some basic back-end tasks.",
+    repository: "https://github.com/nevobit/helebba-monorepo",
+    liveCode: "https://www.helebba.com/",
+    technologies: [
+      "HTML",
+      "CSS",
+      "TypeScript",
+      "React",
+      "React Toolkit",
+      "Next 13",
+      "Node js",
+    ],
+    stackType: "frontend",
+    alert: true,
   },
 ];
